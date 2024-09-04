481620c3fb6f3e36a98a8e12ab0bfa0fff72c2f0
SavionShipp <shippsavion209@yahoo.com>
2024-09-03 18:15:27 -0700

    added show test

--- app/views/products/_product.json.jbuilder
+++ app/views/products/_product.json.jbuilder
@@ -3,3 +3,5 @@ json.name product.name
 json.price product.price
 json.image_url product.image_url
 json.description product.description
+json.created_at product.created_at
+json.updated_at product.updated_at
--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -20,5 +20,13 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     end
   end
 
+  test "show" do
+    get "/product/#{Product.first.id}.json"
+    assert_response 200
+
+    data = JSON.parse(response.body)
+    assert_equal ["id", "name", "price", "image_url", "description", "created_at", "updated_at"], data.keys
+  end
+
   
 end
