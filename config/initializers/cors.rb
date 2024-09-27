15558cd9c536b00701d8fe93ef18d33e1ce34040
SavionShipp <shippsavion209@yahoo.com>
2024-09-26 18:58:38 -0700

    added Signup action

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:5173"
+    origins "localhost:5174"
     resource '*', headers: :any, methods: [:get, :post, :patch, :put]
   end
 end
