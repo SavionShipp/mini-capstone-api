a20d783a962aa6929b6f9b2562716c969600fb23
SavionShipp <shippsavion209@yahoo.com>
2024-10-08 18:48:58 -0700

    created index create and destroy action for cart

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,3 +1,24 @@
 class CartedProductsController < ApplicationController
-  
+  def index
+    @carted_products = CartedProduct.all
+    render :index
+  end
+
+  def create
+    @carted_product = CartedProduct.new(
+      user_id: current_user.id,
+      product_id: params[:product_id],
+      order_id: params[:order_id],
+      quantity: params[:quantity],
+      status: params[:status]
+    )
+    @carted_product.save
+    render :show
+  end
+
+  def destroy
+    @carted_product = CartedProduct.find_by(id: params[:id])
+    @carted_product.destroy
+    render json: {message: "product is gone"}
+  end
 end
--- app/models/carted_product.rb
+++ app/models/carted_product.rb
@@ -1,5 +1,5 @@
 class CartedProduct < ApplicationRecord
   belongs_to:user
   belongs_to:product
-  belongs_to:order
+  belongs_to:order, optional: true
 end
